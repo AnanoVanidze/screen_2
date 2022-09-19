7f9ab4f3c8f314855cffb3f0cbca0a58f9c10f06
Anano <vanidzeanano@gmail.com>
2022-09-19 17:26:16 +0400

    added view binding

--- app/src/main/java/com/example/screen_2/MainActivity.kt
+++ app/src/main/java/com/example/screen_2/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.screen_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.screen_2.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recipientsTv.text = getText(R.string.recipients)
+        binding.transferTv.text = getText(R.string.transfer_details)
+        binding.paymentTv.text = getText(R.string.payment_request)
+        binding.amountTv.text = getText(R.string.amount)
+        binding.amountNumberTv.text = getText(R.string._10_00)
+
+
+
+
+
+
+
     }
 }
